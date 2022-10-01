e1989f974d468f2e7b6b44d7e19928196bff06d7
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2022-10-01 16:13:48 -0700

    filtered en.json to words more than 3 letters

--- src/components/environment/App/App.js
+++ src/components/environment/App/App.js
@@ -11,6 +11,17 @@ const darkTheme = createTheme({
   },
 });
 
+let words = {};
+
+fetch('en.json')
+  .then((response) => response.json())
+  .then((data) => {
+      Object.keys(data).filter(word => word.length > 3).forEach(word => {
+        words[word] = '';
+      })
+      console.log(words, '<-- filtered words');
+  })
+
 function App() {
   return (
     <Provider store={store}>
